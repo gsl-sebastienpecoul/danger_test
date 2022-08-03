91800eb6e1fc37d60ceccd0da38d4c8b685f61a4
Sébastien pécoul <sebastien.pecoul-ext@groupeseloger.com>
2022-08-03 14:20:42 +0200

    Initial Commit

---
+++ Danger_check/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Danger_check
+//
+//  Created by Sebastien PECOUL on 03/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Danger_check/Danger_checkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Danger_checkApp.swift
+//  Danger_check
+//
+//  Created by Sebastien PECOUL on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Danger_checkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
